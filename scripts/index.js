e3d99002b048f48fc23bce89cf734afa4b8ca8b2
Dexter Tan <mr.aves@gmail.com>
2017-12-05 03:06:24 +0800

    Add controls for adding a new topic Add list of topics for display

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,28 @@
-var upDownVoteApp = angular.module('upDownVoteApp', []);
+const upDownVoteApp = angular.module('upDownVoteApp', []);
+const topics = [];
 
-upDownVoteApp.controller('UpDownVoteController', function() {
+upDownVoteApp.controller('UpDownVoteController', function UpDownVoteController($scope) {
+  $scope.newTopic = '';
+  $scope.topics = topics;
+  const topicsHash = {};
+  const initialUpVoteScore = 0;
+  const initialDownVoteScore = 0;
+  const initialTopicScore = initialUpVoteScore - initialDownVoteScore;
+  let topicCounter = 0;
 
+  $scope.addNewTopic = function(newTopicName) {
+    const topic = {
+      id: topicCounter,
+      name: newTopicName,
+      upvotes: initialUpVoteScore,
+      downvotes: initialDownVoteScore,
+      score: initialTopicScore
+    };
+
+    topicsHash[topic.id] = topic;
+    topicCounter++;
+
+    $scope.topics.push(topic);
+    $scope.newTopic = '';
+  }
 });
