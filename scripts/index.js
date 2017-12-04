a837084e1379f0929c1c80feea1d60c260572c9a
Dexter Tan <mr.aves@gmail.com>
2017-12-05 03:34:00 +0800

    Add votable topic directive to contain the topic and its individual upvote and downvote buttons

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,6 @@ const topics = [];
 upDownVoteApp.controller('UpDownVoteController', function UpDownVoteController($scope) {
   $scope.newTopic = '';
   $scope.topics = topics;
-  const topicsHash = {};
   const initialUpVoteScore = 0;
   const initialDownVoteScore = 0;
   const initialTopicScore = initialUpVoteScore - initialDownVoteScore;
@@ -16,13 +15,36 @@ upDownVoteApp.controller('UpDownVoteController', function UpDownVoteController($
       name: newTopicName,
       upvotes: initialUpVoteScore,
       downvotes: initialDownVoteScore,
-      score: initialTopicScore
+      score: initialTopicScore,
+      recalculateScore: function() {
+        this.score = this.upvotes - this.downvotes;
+      }
     };
 
-    topicsHash[topic.id] = topic;
     topicCounter++;
 
     $scope.topics.push(topic);
     $scope.newTopic = '';
+  }
+});
+
+upDownVoteApp.directive('votableTopic', function votableTopic() {
+  return {
+    restrict: 'E',
+    scope: {
+      topic: '=data'
+    },
+    template: '<div>{{topic.score}} <input type="button" value="Up" ng-click="upvote()"><input type="button" value="Down" ng-click="downvote()"> {{topic.name}}</div>',
+    controller: ['$scope', function VotableTopicController($scope) {
+      $scope.upvote = function() {
+        $scope.topic.upvotes++;
+        $scope.topic.recalculateScore();
+      }
+
+      $scope.downvote = function() {
+        $scope.topic.downvotes++;
+        $scope.topic.recalculateScore();
+      }
+    }]
   }
 });
