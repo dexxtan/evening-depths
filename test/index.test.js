f8f1567a07c190cdf756b34d79f65532b9bdb4ae
Dexter Tan <mr.aves@gmail.com>
2017-12-05 23:07:42 +0800

    Add more tests

--- test/index.test.js
+++ test/index.test.js
@@ -14,5 +14,29 @@ describe('UpDownVoteController', function() {
       let controller = $controller('UpDownVoteController', { $scope: $scope });
       expect($scope.newTopic).toEqual('');
     });
+
+    it('sets $scope.topics to be an empty array', function() {
+      let $scope = $rootScope.$new();
+      let controller = $controller('UpDownVoteController', { $scope: $scope });
+      expect($scope.topics instanceof Array).toBe(true);
+      expect($scope.topics.length).toEqual(0);
+    });
+  });
+
+  describe('addNewTopic', function() {
+    it('adds a new topic to $scope.topics with the correct values', function() {
+      const topicName = 'testTopic1';
+      let $scope = $rootScope.$new();
+      let controller = $controller('UpDownVoteController', { $scope: $scope });
+      $scope.newTopic = topicName;
+      $scope.addNewTopic(topicName);
+      expect($scope.topics.length).toEqual(1);
+      expect($scope.topics[0].id).toEqual(0);
+      expect($scope.topics[0].name).toEqual(topicName);
+      expect($scope.topics[0].upvotes).toEqual(0);
+      expect($scope.topics[0].downvotes).toEqual(0);
+      expect($scope.topics[0].score).toEqual(0);
+      expect($scope.newTopic).toEqual('');
+    });
   });
 });
